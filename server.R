31c9830def19b3e596c35a07b449937cd3872f4e
phoebewong <phoebewong@users.noreply.github.com>
2017-06-07 18:14:35 -0400

    Uploading original files

---
+++ server.R
@@ -0,0 +1,331 @@
+# Shiny 2.2
+library(dplyr)
+library(ggplot2)
+library("ggthemes")
+library(scales)
+library(shiny)
+library(data.table)
+library(plotly)
+library(igraph)
+library(networkD3)
+# To do:
+# 1. Remove "Canceled:"
+# 2. Provide a text entry box to remove certain events
+# 3. Show numbers in % (checkbox option)
+# 4. Calculate percentage of work time/given count of work days
+# 5. barplot of events by months
+# 6. Remove Appointment
+# template <- read.csv("./data/df_template.csv", header = TRUE, sep = ",", comment.char="",stringsAsFactors = FALSE)
+
+shinyServer(function(input, output, session) {
+  #output$instruction <- renderText({return( paste("Instruction to download csv file of your calendar","http://tinnes.co.uk/desktopcalendar/support/7/faq_importexport_exportoutlook2010.php"))})
+  
+  meet.df <- reactive({
+    infile <- input$meet.df
+    # if (is.null(infile)) {return(NULL)} # User has not uploaded a file yet
+    if (is.null(infile)) {
+      df <- read.csv("./data/df_template.csv", header = TRUE, sep = ",", comment.char="",stringsAsFactors = FALSE)
+    } else{
+      df <- read.csv(infile$datapath, header = TRUE, sep = ",", comment.char="",stringsAsFactors = FALSE) 
+      }
+    return (df)
+  })
+
+  display.df <- reactive({
+    #validate(need(input$meet.df != "", "Please upload a data set"))
+    display.df <- meet.df()
+    display.df$All.day.event <- ifelse(display.df$All.day.event == "True", TRUE, FALSE)
+    if(input$showallday == TRUE){
+      display.df <- subset(display.df, display.df$All.day.event == FALSE & display.df$Required.Attendees != "" & display.df$Meeting.Organizer != "")
+      }
+    else{display.df <- display.df}
+    display.df$start <- as.POSIXct(paste(display.df$Start.Date, display.df$Start.Time), format = "%m/%d/%Y %I:%M:%S %p")
+    display.df$end   <- as.POSIXct(paste(display.df$End.Date, display.df$End.Time), format = "%m/%d/%Y %I:%M:%S %p")
+    
+    display.df$Start.Date <- as.Date.character(display.df$Start.Date, format = "%m/%d/%Y")
+    display.df$End.Date <- as.Date.character(display.df$End.Date, format = "%m/%d/%Y")
+    
+    display.df$Required.Attendees[display.df$Required.Attendees == ""] <- NA
+    display.df$Optional.Attendees[display.df$Optional.Attendees == ""] <- NA
+    
+    min.date <- input$dates[1]
+    max.date <- input$dates[2]
+    display.df <- subset(display.df, display.df$Start.Date >= min.date & display.df$End.Date <= max.date)
+    
+    ## Cleaning Time ##
+    display.df$duration <- as.numeric(display.df$end-display.df$start)
+    #print(display.df$hour)
+    display.df$hour <- as.numeric(format(display.df$start, "%H")) # unclass((as.POSIXlt(display.df$start)))$hour
+    display.df$wday <- lubridate::wday(display.df$start) - 1 # unclass((as.POSIXlt(display.df$start)))$wday #wday(display.df$start)
+    return(display.df)
+  })
+  # Columns to show (reactive)
+  observe({ 
+    x <- colnames(meet.df())
+    # Can use character(0) to remove all choices
+    if (is.null(x)) x <- character(0)
+    updateSelectizeInput(session, inputId = "show_vars",
+                         label = "Columns to show:",
+                         choices = x,
+                         selected = x[c(1:6, 10:12, 14)])
+  })
+  
+  # Date of data to show (reactive datarangeinput) ####
+  output$dates <- renderUI({
+    df <- meet.df()
+    # print(df)
+    min <- min(as.Date.character(df$Start.Date, format = "%m/%d/%Y"))
+    max <- max(as.Date.character(df$Start.Date, format = "%m/%d/%Y"))
+    dateRangeInput('dates', label = "Date input:",
+                   start = min, end = max,
+                   min = min, max = max)
+  })
+  # observe({ ####
+  #   df <- display.df()
+  #   print(df)
+  #   updateDateRangeInput(session, "dateRange", label = 'Date input: yyyy-mm-dd',
+  #                        start = min(df$Start.Date, na.rm = TRUE),
+  #                        end = max(df$End.Date, na.rm = TRUE))
+  # }) ####
+  output$table <- renderDataTable({
+    #validate(need(input$meet.df != "", "Please upload a data set"))
+    display.df()[, input$show_vars, drop = FALSE]
+  })
+  
+  #output$date <- renderText({format(input$dateRange[1])})
+  # Number of people to show (reactive sliderbar)
+  observe({
+    df <- display.df()
+    n <- length(unique(df$Meeting.Organizer))
+    updateSliderInput(session, "n", value = 20, min = 1, max = n)
+  })
+  
+  output$plot1title <- renderText({
+    #validate(need(input$meet.df != "", "Please upload a data set"))
+    paste("Top", input$n, "people who initaited a meeting with me from", input$dates[1], "to", input$dates[2])})
+  output$plot1 <- renderPlot({
+    #validate(need(input$meet.df != "", ""))
+    meet_clean.df <- display.df()
+    n <- input$n
+    people.freq.df <- as.data.frame(table(meet_clean.df$Meeting.Organizer)) %>%
+      top_n(n, Freq) %>% #show n rows, will show >n rows if there're ties
+      arrange(desc(Freq)) #%>% #show by descending order of Freq
+      # rename(Name=Var1) #-> people.freq.df
+    rot <- 45
+    # sort <- "Freq"
+    
+    # if (sort == "Freq"){
+    people.freq.df$Var1 <- factor(people.freq.df$Var1)
+    people.freq.df$Var1 <- reorder(people.freq.df$Var1, -(people.freq.df$Freq))
+    # } else {
+    #   people.freq.df$Var1 <- people.freq.df$Var1
+    # }
+    # 
+    g <- ggplot(data=people.freq.df, aes(x=Var1, y=Freq)) +
+      geom_bar(stat = "identity") +
+      geom_text(aes(label = Freq), vjust = -0.5) +
+      xlab("Name")+
+      theme(axis.text.x = element_text(angle = rot, hjust = 1))
+    print(g)
+  })
+  output$plot2title <- renderText({
+    #validate(need(input$meet.df != "", ""))
+    paste("Top", input$n, "people who attended a meeting with me from", input$dates[1], "to", input$dates[2])})
+  
+  output$plot2 <- renderPlot({
+    #validate(need(input$meet.df != "", ""))
+    meet.df <- display.df()
+    n <- input$n
+    split.func <- function(x){ return (unlist(strsplit(x, split = ";")))}
+    split2.func <- function(x){ return (strsplit(x, split = ";"))}
+    # To avoid NA as character
+    meeting.org <- as.character(na.omit(unlist(sapply(meet.df$Meeting.Organizer, split.func))))
+    required.attendee <- as.character(na.omit(unlist(sapply(meet.df$Required.Attendees, split.func))))
+    optional.attendee <- as.character(na.omit(unlist(sapply(meet.df$Optional.Attendees, split.func))))
+
+    all.attendee <- c(meeting.org, required.attendee, optional.attendee)
+      
+    people.freq.df <- as.data.frame(table(all.attendee)) %>%
+      top_n(n, Freq) %>% #show n rows, will show >n rows if there're ties
+      arrange(desc(Freq)) #%>% #show by descending order of Freq
+      #rename(Name=all.attendee2) -> people.freq.df
+    rot <- 45
+    # sort <- "Freq" #or by "Alphabet"
+
+    # if (sort == "Freq"){
+      people.freq.df$all.attendee <- factor(people.freq.df$all.attendee)
+      people.freq.df$all.attendee <- reorder(people.freq.df$all.attendee, -(people.freq.df$Freq))
+    # } else {
+    #   people.freq.df$Name <- people.freq.df$Name
+    # 
+
+    g <- ggplot(data=people.freq.df, aes(x=all.attendee, y=Freq)) +
+      geom_bar(stat = "identity") +
+      geom_text(aes(label = Freq), vjust = -0.5) +
+      xlab("Name")+
+      theme(axis.text.x = element_text(angle = rot, hjust = 1)) #+
+    #theme_wsj() #+
+    # scale_colour_few(palette = "medium")
+    print(g)
+  })
+  # output$netplot <- renderPlot({
+  #   
+  # })
+  output$timeplot1 <- renderPlotly({
+    temp <- display.df()
+    temp <- subset(temp, temp$wday >= 1 & temp$wday <= 5)
+    time.df <- as.data.frame(table(temp$wday, temp$hour))
+    colnames(time.df) <- c("wday", "hour", "Freq")
+    time.df$hour <- factor(time.df$hour, levels = levels(time.df$hour)[nlevels(time.df$hour):1])
+    time.df$desc <- character(nrow(time.df))
+    
+    for (i in 1:nlevels(time.df$wday)){
+      for (j in 1:nlevels(time.df$hour)){
+        day <- unique(time.df$wday)[i]
+        hour <- unique(time.df$hour)[j]
+        
+        index <- which(temp$wday == day & temp$hour == hour)
+        desc <- as.character(names(sort(table(temp$Subject[index]), decreasing  = TRUE)))
+        
+        index2 <- as.numeric(which(time.df$wday == day & time.df$hour == hour))
+        time.df$desc[index2] <- desc[1] #show top 3 meeting subject
+      }
+    }
+    if (input$showname == TRUE){
+      g <- ggplot(time.df, aes(x = wday, y = hour, fill = Freq, text = paste("Event:",desc)))
+    }else{ g <- ggplot(time.df, aes(x = wday, y = hour, fill = Freq))}
+    # g <- ggplot(time.df, aes(x = wday, y = hour, fill = Freq, text = paste("Event:",desc)))
+      g <- g + 
+         geom_tile(color="white", size=0.1) +
+        scale_fill_gradient(low = "white", high = "red") + 
+        labs(x="Day", y="Hour", title="Meetings per weekday & time of day")
+        # theme_tufte(base_family="Helvetica")
+
+    return(ggplotly(g))
+    
+  })
+  
+  output$netplot1 <- renderPlot({
+    netplot.df <- display.df()
+    netplot.df$all <- paste(netplot.df$Meeting.Organizer, netplot.df$Required.Attendees, sep = ";")
+    split2.func <- function(x){ return (strsplit(x, split = ";"))}
+    netplot.df$all2 <- sapply(netplot.df$all, split2.func)
+    temp <- unlist(netplot.df$all2) #everyone's name
+    temp <- temp[temp != ""] #manual clean up
+    n <- as.numeric(length(unique(temp)))
+    
+    people.df <- data.frame(matrix(0, ncol = n, nrow = 1))
+    colnames(people.df) <- unique(temp)
+    for (i in 1:ncol(people.df)){
+      for (j in 1:nrow(netplot.df)){
+        people.df[j,i] <- grepl(colnames(people.df)[i], netplot.df$all2[j]) 
+      }
+    }
+    data <- people.df
+    
+    total_occurrences <- colSums(data)
+    index <- total_occurrences >= sort(total_occurrences, decreasing = TRUE)[input$net_n] #median(total_occurrences)
+    data <- subset(data, select = index == TRUE)
+    total_occurrences <- colSums(data)
+    
+    data_matrix <- as.matrix(data)
+    co_occurrence <- t(data_matrix) %*% data_matrix
+    graph <- graph.adjacency(co_occurrence,
+                             weighted=TRUE,
+                             mode="undirected",
+                             diag=FALSE)
+    if(input$random_net == FALSE) {set.seed(33)}
+    return(plot(graph,
+         #vertex.label=names(data),
+         vertex.size=total_occurrences*input$node_size/500,
+         edge.width=E(graph)$weight*input$net_width/1000))
+  })
+  
+  output$netplot2 <- renderForceNetwork({
+    netplot.df <- display.df()
+    netplot.df$all <- paste(netplot.df$Meeting.Organizer, netplot.df$Required.Attendees, sep = ";")
+    split2.func <- function(x){ return (strsplit(x, split = ";"))}
+    netplot.df$all2 <- sapply(netplot.df$all, split2.func)
+    temp <- unlist(netplot.df$all2) #everyone's name
+    temp <- temp[temp != ""] #manual clean up
+    n <- as.numeric(length(unique(temp)))
+    
+    people.df <- data.frame(matrix(0, ncol = n, nrow = 1))
+    colnames(people.df) <- unique(temp)
+    for (i in 1:ncol(people.df)){
+      for (j in 1:nrow(netplot.df)){
+        people.df[j,i] <- grepl(colnames(people.df)[i], netplot.df$all2[j]) 
+      }
+    }
+    data <- people.df
+    
+    total_occurrences <- colSums(data)
+    index <- total_occurrences >= sort(total_occurrences, decreasing = TRUE)[input$net_n] #median(total_occurrences)
+    data <- subset(data, select = index == TRUE)
+    total_occurrences <- colSums(data)
+    
+    data_matrix <- as.matrix(data)
+    co_occurrence <- t(data_matrix) %*% data_matrix
+    graph <- graph.adjacency(co_occurrence,
+                             weighted=TRUE,
+                             mode="undirected",
+                             diag=FALSE)
+    wc <- cluster_walktrap(graph)
+    members <- membership(wc)
+    graph_in <- igraph_to_networkD3(graph, group = members)
+    
+    return(forceNetwork(Links = graph_in$links, Nodes = graph_in$nodes, 
+                 Source = 'source', Target = 'target', 
+                 NodeID = 'name', Group = 'group', zoom = TRUE))
+  })
+  
+  output$summary1 <- renderText({
+    paste("From", input$dates[1], "to", input$dates[2],  "I had", nrow(display.df()), "meetings")})
+  output$summary2 <- renderText({
+    meet.df <- display.df()
+    n <- input$n
+    split.func <- function(x){ return (unlist(strsplit(x, split = ";")))}
+    split2.func <- function(x){ return (strsplit(x, split = ";"))}
+    # To avoid NA as character
+    meeting.org <- as.character(na.omit(unlist(sapply(meet.df$Meeting.Organizer, split.func))))
+    required.attendee <- as.character(na.omit(unlist(sapply(meet.df$Required.Attendees, split.func))))
+    optional.attendee <- as.character(na.omit(unlist(sapply(meet.df$Optional.Attendees, split.func))))
+    
+    all.attendee <- c(meeting.org, required.attendee)
+    
+    people.freq.df <- as.data.frame(table(all.attendee)) %>%
+      top_n(n, Freq) %>% #show n rows, will show >n rows if there're ties
+      arrange(desc(Freq)) #%>% #show by descending order of Freq
+    #rename(Name=all.attendee2) -> people.freq.df
+    rot <- 45
+    # sort <- "Freq" #or by "Alphabet"
+    
+    # if (sort == "Freq"){
+    people.freq.df$all.attendee <- factor(people.freq.df$all.attendee)
+    people.freq.df$all.attendee <- reorder(people.freq.df$all.attendee, -(people.freq.df$Freq))
+    return(paste("Most of them are with", people.freq.df$all.attendee[2]))
+    })
+  output$summary3 <- renderText({
+    meet.df <- display.df()
+    n <- input$n
+    split.func <- function(x){ return (unlist(strsplit(x, split = ";")))}
+    split2.func <- function(x){ return (strsplit(x, split = ";"))}
+    # To avoid NA as character
+    meeting.org <- as.character(na.omit(unlist(sapply(meet.df$Meeting.Organizer, split.func))))
+    required.attendee <- as.character(na.omit(unlist(sapply(meet.df$Required.Attendees, split.func))))
+    optional.attendee <- as.character(na.omit(unlist(sapply(meet.df$Optional.Attendees, split.func))))
+    
+    all.attendee <- c(meeting.org, required.attendee)
+    
+    people.freq.df <- as.data.frame(table(all.attendee)) %>%
+      top_n(n, Freq) %>% #show n rows, will show >n rows if there're ties
+      arrange(desc(Freq)) #%>% #show by descending order of Freq
+    #rename(Name=all.attendee2) -> people.freq.df
+    rot <- 45
+    # sort <- "Freq" #or by "Alphabet"
+    
+    # if (sort == "Freq"){
+    people.freq.df$all.attendee <- factor(people.freq.df$all.attendee)
+    return(paste("To be exact,", round(people.freq.df$Freq[2]/nrow(display.df())* 100), "% of my meetings are with", people.freq.df$all.attendee[2]))
+  })
+})
---
+++ ui.R
@@ -0,0 +1,71 @@
+# Shiny 2.2
+library(shiny)
+library(plotly)
+library(shinythemes)
+library(networkD3)
+
+shinyUI(fluidPage(
+  theme = shinytheme("sandstone"),
+  
+  titlePanel("Statistics of My Meetings"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      textOutput("instruction"),
+      # File input 
+      fileInput('meet.df', 'Choose CSV File',
+                accept=c(#'text/csv', 
+                         #'text/comma-separated-values,text/plain', 
+                         '.csv','.CSV')),
+      # Add a horizontal line
+      tags$hr(), 
+      # Columns to show
+      selectInput('show_vars','Columns to show:', choices = NULL, #placeholder for columns
+                  multiple = TRUE, selectize = TRUE),
+      
+      # Date input
+      uiOutput("dates"),
+      # dateRangeInput('dateRange',
+      #                 label = 'Date input: yyyy-mm-dd',
+      #                 start = Sys.Date() - 7, end = Sys.Date()),
+      # 
+      checkboxInput("showallday", "Exclude All Day Events", value = TRUE)#,
+      #checkboxInput("showModel2", "Show/Hide Model 2", value = TRUE)
+    ),
+    
+    mainPanel(
+      tabsetPanel(type = "tabs", 
+                  tabPanel("Table", dataTableOutput("table"),
+                                    verbatimTextOutput("date") ),
+                  tabPanel("Summary", 
+                           h1(textOutput("summary1")),
+                           h2(textOutput("summary2")),
+                           h3(textOutput("summary3"))),
+                  
+                  tabPanel("Plot: People", 
+                           # Number of people to show
+                           sliderInput("n", "Number of People to Show:",min=1, max=40, value=20),
+                           h2(textOutput("plot1title")),
+                           plotOutput("plot1"),
+                           h2(textOutput("plot2title")),
+                           plotOutput("plot2")),
+                  tabPanel("Plot: Time", plotlyOutput("timeplot1"),
+                           checkboxInput("showname", "Show Event Name", value = TRUE)),
+                  tabPanel("Plot: Network", plotOutput("netplot1"),
+                           checkboxInput("random_net", "Rearrange the plot after changes", value = FALSE),
+                           sliderInput("net_n", "Number of People in the Network:",min=1, max=40, value=5),
+                           sliderInput("net_width", "Link Width:",min=1, max=100, value=20),
+                           sliderInput("node_size", "Node Size:",min=1, max=100, value=50),
+                           forceNetworkOutput("netplot2", width = "100%", height = "500px")) #
+                  
+                  )
+                  
+      # h3("Statistics"),
+      # textOutput("scene"),
+      # plotOutput("plot1")#,
+      # h3("Predicted Horsepower from Model 2:"),
+      # textOutput("pred2"),
+      # textOutput("name")
+    )
+  )
+))
